ddd85688a0b34d9fb449f03af4fcabf3300099ec
rlazcano <rlazcano@medicosoft.mx>
2021-01-11 00:50:46 -0600

    primer commit curso de java se

---
+++ holaMundo/src/main/java/HolaMundo.java
@@ -0,0 +1,12 @@
+/*
+Commentario
+de varias lineas
+*/
+
+//comentario de una sola linea
+public class HolaMundo {
+    
+    public static void main(String args[]){
+       System.out.println("Hola mundo");
+    }
+}
